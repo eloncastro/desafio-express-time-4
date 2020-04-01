e7e02c3c5e2e35a4dcf5a941a1dd42060741fcf2
Élon Castro <elon@ecgroup.com.br>
2020-04-01 15:20:31 -0300

    Criação inicial do adminController, admin.ejs e congituração da rota

---
+++ controllers/adminController.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const fs = require('fs');
+
+const adminController = {
+    index: (req, res) => {
+      res.render('admin', {title: "Admin"});
+    }
+}
+
+module.exports = adminController;
